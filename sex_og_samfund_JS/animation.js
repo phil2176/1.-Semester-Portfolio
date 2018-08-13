dc4bbd46255c5973395ea9997aa6f1a5ec0e6868
Philip Stampe <philipstampe@philips-mbp.home>
2018-08-13 08:48:11 +0200

    Done

---
+++ sex_og_samfund_JS/animation.js
@@ -0,0 +1,56 @@
+$(window).on("load", sidenVises);
+
+function sidenvises(){
+    console. log("sidenvises");
+    //Start natur lyde audio_hero_CityPark_DIGIFX1-17.mp3//
+
+    //Start fodtrin dreng//
+
+    //Start fodtrin pige//
+
+    //Start flytte og gå animation for dreng og pige//
+
+}
+
+
+function valgbillede(){
+    console. log("valgbillede");
+    //Stop fodtrin dreng//
+
+    //Stop fodtrin pige//
+
+    //Stop flytte og gå animation for dreng og pige//
+
+    //Start flytte animation mobil_og_hånd//
+
+    //Tryk på mobil kamera knap//
+    //Ved tryk af knap start tag_billede animation//
+    //Start kameraflash animation//
+
+    //Sæt ind send videre og slet tekstbokse//
+}
+
+function slet(){
+    console. log("slet");
+    //Spil god lyd //
+
+    //Sæt ind tekstboks med du valgte det rigtige valg//
+
+    //Sæt ind tekstboks med det er dårligt at videresende billeder//
+
+    //Sæt ind tekstboks med link til sex og samfunds hjemmeside//
+}
+
+
+function send(){
+    console. log("send");
+    //Spil dårlig lyd//
+
+    //Sæt ind et stil billede af personen der tog billedet som er i fængsel//
+
+    //Sæt ind tekstboks med Du valgte det forkert valg//
+
+    //Sæt ind tekstboks med det er dårligt at videresende billeder//
+
+    //Sæt ind tekstboks med link til sex og samfunds hjemmeside//
+}
